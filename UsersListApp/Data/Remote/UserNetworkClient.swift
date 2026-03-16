c228fa8b0addc8febc896b34a21477fd3ca09476
Nikolay Pochekuev <emeraldvulture@gmail.com>
2026-03-16 18:51:50 +0100

    move NetworkError to separate file

--- UsersListApp/Data/Remote/UserNetworkClient.swift
+++ UsersListApp/Data/Remote/UserNetworkClient.swift
@@ -9,32 +9,6 @@ import Foundation
 
 final class UserNetworkClient {
 
-    enum NetworkError: Error, LocalizedError {
-        case invalidBaseURL
-        case invalidURLComponents
-        case invalidResponse
-        case statusCode(Int)
-        case decoding(Error)
-        case network(Error)
-
-        var errorDescription: String? {
-            switch self {
-            case .invalidBaseURL:
-                "The provided base URL is invalid."
-            case .invalidURLComponents:
-                "Failed to construct a valid URL from components."
-            case .invalidResponse:
-                "The server returned an invalid response."
-            case .statusCode(let code):
-                "Unexpected status code: \(code)."
-            case .decoding(let error):
-                "Failed to decode response: \(error.localizedDescription)"
-            case .network(let error):
-                "Network error: \(error.localizedDescription)"
-            }
-        }
-    }
-
     private let session: URLSession
     private let baseURL = URL(string: "https://randomuser.me")
     
